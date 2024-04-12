a53e14685fd246196fab4da1204709e67d843c00
zevans4 <166666102+zevans4@users.noreply.github.com>
2024-04-12 17:07:42 -0400

    Update amr_lab9.py
    I added the decoder(password) function and added the decoded_password = decoder(encoded_password) line of code. I also changed the "encoded_passwrd" variable to be "encoded_password" because it wasn't consistent with my new variable of "decoded_password".

--- amr_lab9.py
+++ amr_lab9.py
@@ -3,18 +3,17 @@
 Andrew Ruiz
 """
 
-
 def encoder(password):
     if len(password) != 8 or not password.isdigit():
         return "Invalid password"
     encoded = [(int(char) + 3) % 10 for char in password]
     return ''.join(map(str, encoded))
 
-
-"""
-Decode Function Goes Here
-"""
-
+def decoder(password):
+    password_list = [int(i) for i in password]
+    decoded_password_list = [str(num-3) for num in password_list]
+    decoded_password = ''.join(decoded_password_list)
+    return decoded_password
 
 def main():
     while True:
@@ -27,10 +26,11 @@ def main():
 
         if choice == "1":
             password = input("Please enter your password to encode: ")
-            encoded_passwrd = encoder(password)
+            encoded_password = encoder(password)
             print("Your Password has been encoded and stored\n\n")
         elif choice == "2":
-            print(f"""The encoded password is {encoded_passwrd}, and the original password is {password}\n\n""")
+            decoded_password = decoder(encoded_password)
+            print(f"""The encoded password is {encoded_password}, and the original password is {decoded_password}\n\n""")
         elif choice == "3":
             break
         else:
